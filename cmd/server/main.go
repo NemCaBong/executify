55f0e35f4d86c7d264a77371ca516f4b8fa9e10d
Hoàng Nguyễn Minh <nemcabong@gmail.com>
2026-03-16 22:48:49 +0700

    update main file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"github.com/NemCaBong/executify/internal/adapter/api/http"
+	api_http "github.com/NemCaBong/executify/internal/adapter/api/http"
 	"github.com/NemCaBong/executify/internal/adapter/repository"
 	"github.com/NemCaBong/executify/internal/adapter/worker/executor"
 	"github.com/NemCaBong/executify/internal/config"
@@ -19,6 +24,10 @@ func main() {
 		Short: "Executify is a powerful backend app for online judge system",
 		Long:  `A robust Go service to serve API requests for a scalable online judge system.`,
 		Run: func(cmd *cobra.Command, args []string) {
+			// Create context that listens for the interrupt signal from the OS.
+			ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+			defer stop()
+
 			cfg := config.LoadConfig()
 			db := config.NewMySQLConnection(cfg)
 
@@ -27,14 +36,40 @@ func main() {
 
 			submissionSvc := service.NewSubmissionService(repo, exec)
 
-			app := http.NewApp(submissionSvc)
+			app := api_http.NewApp(submissionSvc)
 
 			r := setupRouter(app)
 
-			fmt.Println("Server starting on :" + cfg.ServerPort)
-			if err := r.Run(fmt.Sprintf(":%s", cfg.ServerPort)); err != nil {
-				fmt.Printf("Failed to run server: %v\n", err)
+			srv := &http.Server{
+				Addr:    fmt.Sprintf(":%s", cfg.ServerPort),
+				Handler: r,
 			}
+
+			// Initializing the server in a goroutine so that
+			// it won't block the graceful shutdown handling below
+			go func() {
+				fmt.Printf("Server starting on %s\n", srv.Addr)
+				if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+					fmt.Printf("listen: %v\n", err)
+				}
+			}()
+
+			// Listen for the interrupt signal.
+			<-ctx.Done()
+
+			// Restore default behavior on the interrupt signal and notify user of shutdown.
+			stop()
+			fmt.Println("shutting down gracefully, press Ctrl+C again to force")
+
+			// The context is used to inform the server it has 5 seconds to finish
+			// the request it is currently handling
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+			defer cancel()
+			if err := srv.Shutdown(ctx); err != nil {
+				fmt.Printf("Server forced to shutdown: %v\n", err)
+			}
+
+			fmt.Println("Server exiting")
 		},
 	}
 
@@ -44,7 +79,7 @@ func main() {
 	}
 }
 
-func setupRouter(app *http.App) *gin.Engine {
+func setupRouter(app *api_http.App) *gin.Engine {
 	r := gin.Default()
 	{
 		v1 := r.Group("/api/v1")
