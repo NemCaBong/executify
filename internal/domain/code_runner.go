cd03494977f6dc6eeb009dc2b1b59e3fcba4cecc
nemcabong <nemcabong@gmail.com>
2026-05-20 14:52:22 +0700

    fix: default 1 process is too restricted

--- internal/domain/code_runner.go
+++ internal/domain/code_runner.go
@@ -142,7 +142,7 @@ func (r *CodeRunner) applyExecOptions() {
 	memLimitKB := prob.MemoryLimit * 1024
 	cpuTime := float64(prob.TimeLimit)
 	wallTime := cpuTime * 2.0
-	processes := 1
+	processes := 10
 
 	if prob.CPUTimeLimit != nil {
 		cpuTime = *prob.CPUTimeLimit
